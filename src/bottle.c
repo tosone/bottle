508d6af9ed3663992478a883c8e73d16191f086a
tosone <i@tosone.cn>
2021-09-02 17:20:37 +0800

    feat(*): Add linenoise implemention

--- src/bottle.c
+++ src/bottle.c
@@ -8,8 +8,12 @@
 #include <cmddefine.h>
 #include <command.h>
 #include <error.h>
+#include <linenoise.h>
 
 #define VERSION "v1.0.0"
+
+const char *hostory_file = "history.txt";
+const char *prompt = "\x1b[1;32mbottle\x1b[0m> ";
 
 #define COMMANDS_CHECK(x)           \
   bottle_error_t result = x;        \
@@ -26,6 +30,9 @@ void handle_signals(int signo) {
 }
 
 int main(int argc, char const *argv[]) {
+  linenoiseHistoryLoad("history.txt");
+  linenoiseHistorySetMaxLen(MAX_LINE);
+
   atexit(clear);
 
   if (signal(SIGINT, handle_signals) == SIG_ERR) {
@@ -33,65 +40,66 @@ int main(int argc, char const *argv[]) {
     return EXIT_FAILURE;
   }
 
-  char buf[MAX_LINE] = {0};
-  while (true) {
-    printf("> ");
-    fflush(stdout);
-    if (fgets(buf, MAX_LINE, stdin) == NULL)
-      return EXIT_FAILURE;
-    size_t len = strlen(buf);
-    while (len && (buf[len - 1] == '\n' || buf[len - 1] == '\r')) {
-      len--;
-      buf[len] = '\0';
-    }
-    int commands_length;
-    commands_t commands = commands_parse(buf, &commands_length);
+  char *line;
+  while ((line = linenoise(prompt)) != NULL) {
+    if (line[0] != '\0') {
+      linenoiseHistoryAdd(line);
+      linenoiseHistorySave("history.txt");
 
-    if (commands_length >= 1) {
-      if (strequal(commands[0], COMMAND_HELP)) {
-        COMMANDS_CHECK(cli_help(commands, commands_length));
-      } else if (strequal(commands[0], COMMAND_EXIT)) {
-        commands_free(commands, commands_length);
-        return EXIT_SUCCESS;
-      } else if (strequal(commands[0], COMMAND_VERSION)) {
-        printf("%s\n", VERSION);
-      } else if (strequal(commands[0], COMMAND_PI)) {
-        COMMANDS_CHECK(cli_pi(commands, commands_length));
-      } else if (strequal(commands[0], COMMAND_HMAP)) {
-        COMMANDS_CHECK(cli_hmap(commands, commands_length));
-      } else if (strequal(commands[0], COMMAND_LRU)) {
-        COMMANDS_CHECK(cli_lru(commands, commands_length));
-      } else if (strequal(commands[0], COMMAND_AVL)) {
-        COMMANDS_CHECK(cli_avl(commands, commands_length));
-      } else if (strequal(commands[0], COMMAND_SKLIST)) {
-        COMMANDS_CHECK(cli_sklist(commands, commands_length));
-      } else if (strequal(commands[0], COMMAND_RBTREE)) {
-        COMMANDS_CHECK(cli_rbtree(commands, commands_length));
-      } else if (strequal(commands[0], COMMAND_DEQUE)) {
-        COMMANDS_CHECK(cli_deque(commands, commands_length));
-      } else if (strequal(commands[0], COMMAND_STACK)) {
-        COMMANDS_CHECK(cli_stack(commands, commands_length));
-      } else if (strequal(commands[0], COMMAND_BLOOM)) {
-        COMMANDS_CHECK(cli_bloom(commands, commands_length));
-      } else if (strequal(commands[0], COMMAND_DAG)) {
-        COMMANDS_CHECK(cli_dag(commands, commands_length));
-      } else if (strequal(commands[0], COMMAND_BITTREE)) {
-        COMMANDS_CHECK(cli_bittree(commands, commands_length));
-      } else if (strequal(commands[0], COMMAND_TRIE)) {
-        COMMANDS_CHECK(cli_trie(commands, commands_length));
-      } else if (strequal(commands[0], COMMAND_HLL)) {
-        COMMANDS_CHECK(cli_hll(commands, commands_length));
-      } else if (strequal(commands[0], COMMAND_LEVENSHTEIN)) {
-        COMMANDS_CHECK(cli_levenshtein(commands, commands_length));
-      } else if (strequal(commands[0], COMMAND_BITARRAY)) {
-        COMMANDS_CHECK(cli_bitarray(commands, commands_length));
-      } else if (strequal(commands[0], COMMAND_SORT)) {
-        COMMANDS_CHECK(cli_sort(commands, commands_length));
-      } else {
-        printf("%s\n", errstr(error_invalid_command));
+      char *line_copy = (char *)malloc(sizeof(char) * (strlen(line) + 1));
+      memcpy(line_copy, line, strlen(line) + 1);
+      int commands_length;
+      commands_t commands = commands_parse(line_copy, &commands_length);
+      free(line_copy);
+
+      if (commands_length >= 1) {
+        if (strequal(commands[0], COMMAND_HELP)) {
+          COMMANDS_CHECK(cli_help(commands, commands_length));
+        } else if (strequal(commands[0], COMMAND_EXIT)) {
+          commands_free(commands, commands_length);
+          free(line);
+          return EXIT_SUCCESS;
+        } else if (strequal(commands[0], COMMAND_VERSION)) {
+          printf("%s\n", VERSION);
+        } else if (strequal(commands[0], COMMAND_PI)) {
+          COMMANDS_CHECK(cli_pi(commands, commands_length));
+        } else if (strequal(commands[0], COMMAND_HMAP)) {
+          COMMANDS_CHECK(cli_hmap(commands, commands_length));
+        } else if (strequal(commands[0], COMMAND_LRU)) {
+          COMMANDS_CHECK(cli_lru(commands, commands_length));
+        } else if (strequal(commands[0], COMMAND_AVL)) {
+          COMMANDS_CHECK(cli_avl(commands, commands_length));
+        } else if (strequal(commands[0], COMMAND_SKLIST)) {
+          COMMANDS_CHECK(cli_sklist(commands, commands_length));
+        } else if (strequal(commands[0], COMMAND_RBTREE)) {
+          COMMANDS_CHECK(cli_rbtree(commands, commands_length));
+        } else if (strequal(commands[0], COMMAND_DEQUE)) {
+          COMMANDS_CHECK(cli_deque(commands, commands_length));
+        } else if (strequal(commands[0], COMMAND_STACK)) {
+          COMMANDS_CHECK(cli_stack(commands, commands_length));
+        } else if (strequal(commands[0], COMMAND_BLOOM)) {
+          COMMANDS_CHECK(cli_bloom(commands, commands_length));
+        } else if (strequal(commands[0], COMMAND_DAG)) {
+          COMMANDS_CHECK(cli_dag(commands, commands_length));
+        } else if (strequal(commands[0], COMMAND_BITTREE)) {
+          COMMANDS_CHECK(cli_bittree(commands, commands_length));
+        } else if (strequal(commands[0], COMMAND_TRIE)) {
+          COMMANDS_CHECK(cli_trie(commands, commands_length));
+        } else if (strequal(commands[0], COMMAND_HLL)) {
+          COMMANDS_CHECK(cli_hll(commands, commands_length));
+        } else if (strequal(commands[0], COMMAND_LEVENSHTEIN)) {
+          COMMANDS_CHECK(cli_levenshtein(commands, commands_length));
+        } else if (strequal(commands[0], COMMAND_BITARRAY)) {
+          COMMANDS_CHECK(cli_bitarray(commands, commands_length));
+        } else if (strequal(commands[0], COMMAND_SORT)) {
+          COMMANDS_CHECK(cli_sort(commands, commands_length));
+        } else {
+          printf("%s\n", errstr(error_invalid_command));
+        }
       }
+      commands_free(commands, commands_length);
     }
-    commands_free(commands, commands_length);
+    free(line);
   }
   return EXIT_SUCCESS;
 }
